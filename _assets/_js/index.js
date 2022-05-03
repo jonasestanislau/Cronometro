ec54f5d5a078b505f39c3d08384e19943f731fad
jonasestanislau <jonas.redes21@gmail.com>
2022-05-02 23:24:20 -0300

    Arquivo
    Necessário ajustar lógica e reduzir o código

--- _assets/_js/index.js
+++ _assets/_js/index.js
@@ -2,6 +2,10 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 let milesimos = 0;
+let btnStart = document.querySelector('.start');
+let btnStop = document.querySelector('.stop');
+let intervalo;
+let statusBtnClick = 0;
 
 function cronometro() {
     document.querySelector('.milesimo').innerHTML = String(milesimos++).padStart(2,'0');
@@ -21,5 +25,26 @@ function cronometro() {
             document.querySelector('.hours').innerHTML = String(hours).padStart(2,'0');
         }
     }
-setInterval(cronometro,10);
+
+btnStart.addEventListener('click', () => {
+    if(statusBtnClick == 1) {
+        clearInterval(intervalo);
+        document.querySelector('p.start').innerHTML = '<i class="fa-solid fa-play"></i>';
+        statusBtnClick = 0;
+    }
+    else {
+        intervalo = setInterval(cronometro,10);
+        document.querySelector('p.start').innerHTML = '<i class="fa-solid fa-pause"></i>';
+        statusBtnClick = 1;
+    }
+})
+
+btnStop.addEventListener('click', () => {
+    clearInterval(intervalo);
+    document.querySelector('.hours').innerHTML = '00'
+    document.querySelector('.minutes').innerHTML = '00'
+    document.querySelector('.seconds').innerHTML = '00'
+    document.querySelector('.milesimo').innerHTML = '00'
+    document.querySelector('p.start').innerHTML = '<i class="fa-solid fa-play"></i>';
+})
 
