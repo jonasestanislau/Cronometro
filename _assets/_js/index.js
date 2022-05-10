005f8017ef42afad8dd1e5800c0ee98b2bb88758
jonasestanislau <jonas.redes21@gmail.com>
2022-05-10 17:43:27 -0300

    Arquivo teste
    teste

--- _assets/_js/index.js
+++ _assets/_js/index.js
@@ -1,50 +1,39 @@
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
-let milesimos = 0;
+// Selecionando os elementos
+let labelMilliSeconds = document.querySelector('.milliSeconds');
+let labelSeconds = document.querySelector('.seconds');
+let labelMinutes = document.querySelector('.minutes');
+let labelHours = document.querySelector('.hours');
 let btnStart = document.querySelector('.start');
 let btnStop = document.querySelector('.stop');
-let intervalo;
-let statusBtnClick = 0;
 
-function cronometro() {
-    document.querySelector('.milesimo').innerHTML = String(milesimos++).padStart(2,'0');
-        if(milesimos == 100) {
-            seconds += 1;
-            milesimos = 0;
-            document.querySelector('.seconds').innerHTML = String(seconds).padStart(2,'0');
-        }
-        else if(seconds == 60) {
-            minutes += 1
-            seconds = 0;
-            document.querySelector('.minutes').innerHTML = String(minutes).padStart(2,'0');
-        }
-        else if(minutes == 60) {
-            hours += 1
-            minutes = 0;
-            document.querySelector('.hours').innerHTML = String(hours).padStart(2,'0');
-        }
+// Atribuindo o valor 0 para os elementos acima
+labelMilliSeconds.value = 0;
+labelSeconds.value = 0;
+labelMinutes.value = 0;
+labelHours.value = 0;
+
+function stopwatch() {
+    labelMilliSeconds.innerHTML = String(labelMilliSeconds.value++).padStart(2,'0');
+    if(labelMilliSeconds.value == 101) {
+        labelSeconds.value += 1;
+        labelSeconds.innerHTML = String(labelSeconds.value).padStart(2,'0');
+        labelMilliSeconds.value = 0;
     }
+    if(labelSeconds.value == 60) {
+        labelMinutes.value += 1;
+        labelMinutes.innerHTML = String(labelMinutes.value).padStart(2,'0');
+        labelSeconds.value = 0;
+    }
+    if(labelMinutes.value == 60) {
+        labelMinutes.value = 0;
+        labelMinutes.innerHTML = String(labelMinutes.value).padStart(2,'0');
+        labelHours.value += 1;
+        labelHours.innerHTML = String(labelHours.value).padStart(2,'0');
+    }
+ }
+
+// let txt = setInterval(stopwatch,1000);
 
 btnStart.addEventListener('click', () => {
-    if(statusBtnClick == 1) {
-        clearInterval(intervalo);
-        document.querySelector('p.start').innerHTML = '<i class="fa-solid fa-play"></i>';
-        statusBtnClick = 0;
-    }
-    else {
-        intervalo = setInterval(cronometro,10);
-        document.querySelector('p.start').innerHTML = '<i class="fa-solid fa-pause"></i>';
-        statusBtnClick = 1;
-    }
+    setInterval(stopwatch,1000)
 })
-
-btnStop.addEventListener('click', () => {
-    clearInterval(intervalo);
-    document.querySelector('.hours').innerHTML = '00'
-    document.querySelector('.minutes').innerHTML = '00'
-    document.querySelector('.seconds').innerHTML = '00'
-    document.querySelector('.milesimo').innerHTML = '00'
-    document.querySelector('p.start').innerHTML = '<i class="fa-solid fa-play"></i>';
-})
-
